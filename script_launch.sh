6c4a3d240098e782c210c5812898b81d3828f811
Maurizio Palmieri <maupalm88@gmail.com>
2023-11-20 16:05:18 +0100

    Update script_launch.sh

--- script_launch.sh
+++ script_launch.sh
@@ -1,3 +1,3 @@
 wget -q -P .  https://github.com/INTO-CPS-Association/maestro/releases/download/Release%2F1.0.10/coe-1.0.10-jar-with-dependencies.jar
 
-java -jar coe-1.0.10-jar-with-dependencies.jar -o -c Multi-models/Voronoi-nogui/co-sim/R_2023_10_31-15_10_28_248/config.json --starttime 0 --endtime 200
+java -jar coe-1.0.10-jar-with-dependencies.jar -o -c Multi-models/standard platooing/co-sim/test/config.json --starttime 0 --endtime 100
