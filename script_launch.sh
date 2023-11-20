9437b7820dae8980c1c73e12998662258778f6c2
Maurizio Palmieri <maupalm88@gmail.com>
2023-11-20 16:58:56 +0100

    Delete script_launch.sh

--- script_launch.sh
+++
@@ -1,3 +0,0 @@
-wget -q -P .  https://github.com/INTO-CPS-Association/maestro/releases/download/Release%2F1.0.10/coe-1.0.10-jar-with-dependencies.jar
-
-java -jar coe-1.0.10-jar-with-dependencies.jar -o -c Multi-models/standard platooing/co-sim/test/config.json --starttime 0 --endtime 100
